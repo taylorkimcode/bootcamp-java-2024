7c7c0119e1997cea4a2cceca5b0896f543fa3367
Jaehyunkim <skdmlakxn45@naver.com>
2024-12-16 21:47:06 +0900

    create : 프로젝트 생성

---
+++ src/main/java/com/taylor/camp/bootcamp_java_2024/BootcampJava2024Application.java
@@ -0,0 +1,13 @@
+package com.taylor.camp.bootcamp_java_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampJava2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampJava2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/taylor/camp/bootcamp_java_2024/BootcampJava2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taylor.camp.bootcamp_java_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampJava2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
